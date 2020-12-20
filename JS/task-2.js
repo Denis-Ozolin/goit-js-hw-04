7b88d0bc1c25eb2e246daee11a8e4a4a3cc8e2fd
Denis-Ozolin <ozolindenx@gmail.com>
2020-12-20 22:14:51 +0200

    homework 4 is finished.

--- JS/task-2.js
+++ JS/task-2.js
@@ -1,10 +1,30 @@
-const countProps = (obj) => Object.entries(obj).length;
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(countProps({})); // 0
+    inventory.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
 
-console.log(countProps({ name: 'Mango', age: 2 })); // 2
+    inventory.items = inventory.items.filter(item => item !== itemName);
+  },
+};
 
-console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
